51cb259b21dcd927eab56d2e4a3e6d87bf7320e9
thollaback <Troy@Troy-PC.(none)>
2013-08-08 18:05:51 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TroyHolleman::Application.config.secret_token = '06e1d078e26a8a983573efcbcdc63c3543e0ac6aabc577cefb9f210f7dbc3549bb9b0dbd7d4942faa621655d328cffa426dc5f9b4f7b4b014e1dc3a9992e9a90'
